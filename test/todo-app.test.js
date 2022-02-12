410b7408edcdf687f475262b35ca4abe30b4c625
Johann <62679700+johannh22@users.noreply.github.com>
2022-02-12 13:10:20 -0300

    Adicionando configurações

---
+++ test/todo-app.test.js
@@ -0,0 +1,14 @@
+const test = require('tape');     // https://github.com/dwyl/learn-tape
+const fs = require('fs');         // to read html files (see below)
+const path = require('path');     // so we can open files cross-platform
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'));
+require('jsdom-global')(html);    // https://github.com/rstacruz/jsdom-global
+const app = require('../lib/todo-app.js');  // functions to test
+const id = 'test-app';            // all tests use 'test-app' as root element
+
+test('todo `model` (Object) has desired keys', (t) => {
+  const keys = Object.keys(app.model);
+  t.deepEqual(keys, ['todos', 'hash'], "`todos` and `hash` keys are present.");
+  t.true(Array.isArray(app.model.todos), "model.todos is an Array");
+  t.end();
+});
